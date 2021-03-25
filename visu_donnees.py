f05c99acac5dfff30f9081b488762e77e8191ee7
clement <clement.cornu@cpe.fr>
2021-03-25 13:30:42 +0100

    modif visu données en extraction

--- visu_donnees.py
+++ visu_donnees.py
@@ -5,14 +5,19 @@ Created on Wed Mar 24 13:17:00 2021
 
 @author: clementcornu
 """
-import sys
-sys.path.insert(0,"/lib/python3/dist-packages/numpy")
-import json,numpy,math,cgitb,cgi
+
+import json,numpy,math,os,time
 from pandas import json_normalize
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plot
 from sklearn.cluster import KMeans
+
+def get_name(image):
+    nom = image.split("/",1)[1]
+    nom = nom.split(".",1)[0]
+    nom = nom+".png"
+    return nom
 
 def x_en_fonction_de_y(x,y):
     data = json.load(open('data.json'))
@@ -42,10 +47,10 @@ def composition_image(image):
     plot.clf()
     imgfile.close()
     
-def clusters_image(image,n):
+def clusters_image(image):
     imgfile = Image.open(image)
     numarray = numpy.array(imgfile.getdata(), numpy.uint8)
-    clusters = KMeans(n_clusters = n)
+    clusters = KMeans(n_clusters = 4)
     clusters.fit(numarray)
     npbins = numpy.arange(0, 5)
     histogram = numpy.histogram(clusters.labels_, bins=npbins)
@@ -57,50 +62,15 @@ def clusters_image(image,n):
             math.ceil(clusters.cluster_centers_[i][1]),
         math.ceil(clusters.cluster_centers_[i][2])))
     juste_le_nom = get_name(image)
-    plot.savefig("Donnees_visualisees/"+str(n)+"_clusters_"+juste_le_nom)
+    plot.savefig("Donnees_visualisees/clusters_"+juste_le_nom)
     plot.clf()
-    
-def get_name(image):
-    nom = image.split("/",1)[1]
-    nom = nom.split(".",1)[0]
-    nom = nom+".png"
-    return nom
-    
-cgitb.enable()
-form = cgi.FieldStorage()
-images_choisies = []
-affichage_images = ""
 
-for i in range(10):
-    if form.getvalue("image_"+str(i)):
-        images_choisies.append(form.getvalue("image_"+str(i)))
-if form.getvalue("visu_type"):
-    visu_type = form.getvalue("visu_type")
-else:
-    visu_type = "compo"
-
-if visu_type == "compo":
-    for i in len(images_choisies)-1:
-        composition_image(images_choisies[i])
-        nom = get_name(images_choisies[i])
-        affichage_images = affichage_images +"<img src='Donnees_visualisees/"+nom+"'>"
-elif visu_type == "clusters":
-    for i in len(images_choisies)-1:
-        clusters_image(images_choisies[i],2)
-        nom = get_name(images_choisies[i])
-        affichage_images = affichage_images +"<img src='Donnees_visualisees/"+nom+"'>"
-else:
-    x_en_fonction_de_y("theme","nom")
-        
-"Affichage du code HTML"   
-print("Content-type: text/html; charset=utf-8\n")
-html ="""
-    <html>
-        <body>
-        
-        """   
-html = html + affichage_images + """
-        <body>
-    </html>
-    """
-print(html)
+start = time.time()   
+for i in os.listdir("Images"):
+    composition_image("Images/"+i)
+    clusters_image("Images/"+i)
+    print("Infos "+i+" synthétisées")
+finish = time.time()
+temps_exec = finish-start
+print("Fin de synthétisation des données")
+print("Le temps de calcul par image était de "+str(temps_exec/10)+" secondes")
